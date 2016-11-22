27266d506066ebf6804b035375f80cd07ba42db4
raditotev <raditotev@gmail.com>
2016-11-22 20:58:31 +0000

    Include extra fields for user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+
+
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :city, :avatar])
+  end
 end
