6f2cf2704b43f6ffcf5b6a5efb9cec503ea29333
raditotev <raditotev@gmail.com>
2016-11-23 09:34:35 +0000

    Add model tests for friendship

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user_one) { create(:user) }
+  let!(:user_two) { create(:user) }
+
+  subject { described_class.new(user: user_one, friend: user_two) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a user" do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+   it "is not valid without a friend" do
+    subject.friend = nil
+    expect(subject).to_not be_valid
+  end
+
+  # it "is not valid if user is same as friend" do
+  #   subject.friend = user_one
+  #   expect(subject.save).to_not be_valid
+  # end
+
+  describe "Associations" do
+    it "belongs to user" do
+      assc = described_class.reflect_on_association(:user)
+      expect(assc.macro).to eq :belongs_to
+    end
+
+    it "belongs to friend" do
+      assc = described_class.reflect_on_association(:friend)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
 end
